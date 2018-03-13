20c6bb7525ded8ce114117354b5500dffebf8b50
Dantu <vamshidhar.d.k@gmail.com>
2018-03-13 16:06:01 -0700

    Modified the example code

--- examples/lstm_ptb/lstm_ptb_service.py
+++ examples/lstm_ptb/lstm_ptb_service.py
@@ -11,8 +11,7 @@ class MXNetLSTMService(MXNetBaseService):
     from length 0 to 60 and generates a sentence with the same size.
     """
     def __init__(self, model_name, model_dir, manifest, gpu=None):
-        super(MXNetLSTMService, self).__init__(model_name, model_dir, manifest, gpu)
-
+        self.model_name = model_name
         self.ctx = mx.gpu(int(gpu)) if gpu is not None else mx.cpu()
         signature_file_path = os.path.join(model_dir, manifest['Model']['Signature'])
         if not os.path.isfile(signature_file_path):
